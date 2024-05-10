2956985b08798d049999189e35a1a8397906619e
aslam <aslam167271@example.com>
2024-05-10 21:59:27 +0530

    folder added

---
+++ SimpleOOPsGame/src/aslam/Main.java
@@ -0,0 +1,13 @@
+package aslam;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Player1 player = new Player1("Aslam","sword",100);
+		//        System.out.println(player.getName());
+		//        System.out.println(player.getHealth());
+		//        System.out.println(player.getWeapon());
+
+
+	}
+}
